f20ff33f517a671e7aab884af51d26d9719db845
Gabriel Grenier <gabriel.braun-grenier@hotmail.com>
2020-05-31 23:29:51 -0400

    deleted useless page + new home section + smooth scroll btn

--- ArtiStats/resources/views/pages/home.blade.php
+++ ArtiStats/resources/views/pages/home.blade.php
@@ -27,12 +27,44 @@
                 <div class="row">
                     <div class="col-lg-4"><a href="#" class="parallaxLink">Word Statistics</a></div>
                     <div class="col-lg-4"><a href="#" class="parallaxLink">Open Source</a></div>
-                    <div class="col-lg-4"><a href="#" class="parallaxLink">Artist Information</a></div>
+                    <div class="col-lg-4"><a href="#ArtistInformation" class="parallaxLink">Artist Information</a></div>
                 </div>
             </div>
         </div>
     </div>
-    <h1>TEST</h1>
 
+    <div class="container section-white" id="ArtistInformation">
+        <div class="row my-5">
+            <div class="col-md-5">
+                <h1>All of your favorite artists</h1>
+                <p>
+                    We are using Musixmatch's API to bring
+                    all the informations about your favorite artists.
+                </p>
+            </div>
+            <div class="col-md-7">
+                <img src="{{asset('images/x17Homepage.png')}}" class="w-100"/>
+            </div>
+        </div>
+    </div>
+    <!-- add call to action -->
 </body>
 @endsection
+@push('scripts')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("a").on('click', function(event) {
+                if (this.hash !== "") {
+                    event.preventDefault();
+                    var hash = this.hash;
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800, function(){
+                        window.location.hash = hash;
+                    });
+                }
+            });
+        });
+    </script>
+@endpush
